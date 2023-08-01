1f95799e850478989b2882df9528a8609bfdef06
StphnX <136083929+StphnX@users.noreply.github.com>
2023-08-01 16:24:56 +0200

    added grid to sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
           <input type="text" name='search' id='search' placeholder='Search for something' />
           <button>Search</button>
         </form>
-        <article>
+        <article className='title'>
           <h1>Very big title here</h1>
           <a href="">Read Full Article</a>
         </article>
 
-        <article>
+        <article className='cards'>
           <div>
             <h2>Heading 2</h2>
             <ul>
@@ -24,6 +24,16 @@ function App() {
             </ul>
             <p>Date</p>
           </div>
+
+          <div>
+            <h2>Heading 2</h2>
+            <ul>
+              <li>By Author XY</li>
+              <li><a href="">Read Full Article</a></li>
+            </ul>
+            <p>Date</p>
+          </div>
+
         </article>
       </section>
 
