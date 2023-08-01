0c7b8a4e033f86cbe616987cc8280b326a79a1c8
Zjotjawik <Zjotjawik@gmail.com>
2023-08-01 16:07:34 +0200

    api implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from "react";
+import {format} from "date-fns";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("programming");
+  const [text, setText] = useState("");
+  const [largeTitle, setLargeTitle] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async() => {
+      const res = await fetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
+      const data = await res.json()
+      setItems(data.hits)
+      setLargeTitle(data.hits[0])
+    }
+
+    fetchArticles()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="section">
+      <form autoComplete="off">
+        <input  type="text" name="search" id="search" placeholder="Search for something"/>
+        <button>Search</button>
+      </form>
+
+        <article className="title">
+          <h1>{largeTitle.title}</h1>
+          <a href="">Read Full Article</a>
+        </article>
+
+        <article className="cards">
+          <div>
+            <h2>Heading 2</h2>
+            <ul>
+              <li>By Sankara</li>
+              <li><a href="">Read Full Article</a></li>
+            </ul>
+            <p>Date</p>
+          </div>
+        </article>
+      </section>
     </>
   )
 }
